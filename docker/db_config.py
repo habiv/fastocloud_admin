ad949079c69d642b0a1c5ad57dace8bdddd8cbbe
topilski <atopilski@gmail.com>
2020-03-04 13:43:37 -0500

    Docker fixes

--- docker/db_config.py
+++ docker/db_config.py
@@ -1,7 +1,3 @@
-MONGO_DBNAME = 'iptv'
-MONGO_URI = 'mongodb://mongodb:27017/' + MONGO_DBNAME
-
-MONGODB_SETTINGS = {
-    'db': MONGO_DBNAME,
-    'host': MONGO_URI
-}
+MONGODB_DB_NAME = 'iptv'
+MONGODB_HOST = 'mongodb'
+MONGODB_PORT = '27017'
